936a3a95856cc05abbc7660284b45fe2ec79ce4e
kaleshovva <72760173+kaleshovva@users.noreply.github.com>
2020-12-23 16:50:28 +0300

    init commit

---
+++ src/app/components/tutorial-details/tutorial-details.component.ts
@@ -0,0 +1,97 @@
+import { Component, OnInit } from '@angular/core';
+import { TutorialService } from 'src/app/services/tutorial.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+interface ITutorial {
+  id: number,
+  data: {
+    title: string,
+    description: string
+  },
+  published?: any
+}
+
+@Component({
+  selector: 'app-tutorial-details',
+  templateUrl: './tutorial-details.component.html',
+  styleUrls: ['./tutorial-details.component.scss']
+})
+
+export class TutorialDetailsComponent implements OnInit {
+  currentTutorial: ITutorial = {
+    id: 0,
+    data: {
+      title: '',
+      description: '',
+    }
+  };
+  message = '';
+
+  constructor(
+    private tutorialService: TutorialService,
+    private route: ActivatedRoute,
+    private router: Router) { }
+
+  ngOnInit(): void {
+    this.message = '';
+    this.getTutorial(this.route.snapshot.paramMap.get('id'));
+  }
+
+  getTutorial(id: string | null): void {
+    this.tutorialService.get(id)
+      .subscribe(
+        data => {
+          this.currentTutorial = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  updatePublished(status: any): void {
+    const data = {
+      title: this.currentTutorial.data.title,
+      description: this.currentTutorial.data.description,
+      published: status
+    };
+
+    this.tutorialService.update(this.currentTutorial.id, data)
+      .subscribe(
+        response => {
+          this.currentTutorial.published = status;
+          console.log(response);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  updateTutorial(): void {
+    this.tutorialService.update(this.currentTutorial.id, { 
+      title: this.currentTutorial.data.title, 
+      description: this.currentTutorial.data.description, 
+      published: this.currentTutorial.published 
+    })
+      .subscribe(
+        response => {
+          console.log(response);
+          this.message = 'The tutorial was updated successfully!';
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  deleteTutorial(): void {
+    this.tutorialService.delete(this.currentTutorial.id)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.router.navigate(['/tutorials']);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+}
---
+++ src/app/components/tutorials-list/tutorials-list.component.ts
@@ -0,0 +1,87 @@
+import { Component, OnInit } from '@angular/core';
+import { TutorialService } from 'src/app/services/tutorial.service';
+
+interface ITutorial {
+  id: number,
+  data: {
+    title: string,
+    description: string
+  },
+  published?: any
+}
+
+@Component({
+  selector: 'app-tutorials-list',
+  templateUrl: './tutorials-list.component.html',
+  styleUrls: ['./tutorials-list.component.scss']
+})
+export class TutorialsListComponent implements OnInit {
+
+  tutorials: any;
+  currentTutorial: ITutorial = this.setEmptyTutorial();
+  currentIndex = -1;
+  title = '';
+
+  constructor(private tutorialService: TutorialService) { }
+
+  ngOnInit(): void {
+    this.retrieveTutorials();
+  }
+
+  retrieveTutorials(): void {
+    this.tutorialService.getAll()
+      .subscribe(
+        data => {
+          this.tutorials = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  private setEmptyTutorial(): ITutorial {
+    return {
+      id: -1,
+      data: {
+        title: '',
+        description: '',
+      }
+    };
+  }
+
+  refreshList(): void {
+    this.retrieveTutorials();
+    this.currentTutorial = this.setEmptyTutorial();
+    this.currentIndex = -1;
+  }
+
+  setActiveTutorial(tutorial: any, index: any): void {
+    this.currentTutorial = tutorial;
+    this.currentIndex = index;
+  }
+
+  removeAllTutorials(): void {
+    this.tutorialService.deleteAll()
+      .subscribe(
+        response => {
+          console.log(response);
+          this.retrieveTutorials();
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  searchTitle(): void {
+    this.tutorialService.findByTitle(this.title)
+      .subscribe(
+        data => {
+          this.tutorials = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+}
